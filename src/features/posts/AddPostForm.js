383100aacc09bc0534c11d13574f09fe14bd3140
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-09-23 18:11:44 +0500

    [Start working on CSS]

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,23 +1,24 @@
 import React from "react";
+import "./post.css";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { postAdded } from "./postSlice";
 import { selectAllUsers } from "../users/usersSlice ";
 const AddPostForm = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [userId, setUserId] = useState("");
 
-  
   const onTitleChange = (e) => setTitle(e.target.value);
   const onContentChange = (e) => setContent(e.target.value);
   const onAuthorChange = (e) => setUserId(e.target.value);
-  
+
   const users = useSelector(selectAllUsers);
 
   const onSavePostClicked = () => {
-    if (title && content && userId) { // Check if all fields have values
+    if (title && content && userId) {
+      // Check if all fields have values
       dispatch(postAdded(title, content, userId));
       setTitle("");
       setContent("");
@@ -35,37 +36,53 @@ const AddPostForm = () => {
 
   return (
     <>
-      <h2>Add a new post</h2>
-      <form>
-        <label htmlFor="postTitle">Post Title</label>
-        <input
-          type="text"
-          id="postTitle"
-          name="postTitle"
-          value={title}
-          onChange={onTitleChange}
-        />
-        <label htmlFor="postAuthor">Author:</label>
-        <select
-          id="postAuthor"
-          value={userId}
-          onChange={onAuthorChange}
-        >
-          <option value=""></option>
-          {usersOptions}
-        </select>
-        <label htmlFor="postContent">Content</label>
-        <input
-          type="text"
-          id="postContent"
-          name="postContent"
-          value={content}
-          onChange={onContentChange}
-        />
-      </form>
-      <button disabled={!canSave} onClick={onSavePostClicked} type="button">
-        Save Post
-      </button>
+      <h2 className="main-heading">Add a new post</h2>
+      <div className="main-div">
+        <form>
+          <div>
+            <label className="post-title" htmlFor="postTitle">
+              Post Title
+            </label>
+            <input
+              type="text"
+              id="postTitle"
+              name="postTitle"
+              value={title}
+              onChange={onTitleChange}
+              className="post-title-input"
+            />
+          </div>
+          <div>
+            <label className="author-name" htmlFor="postAuthor">
+              Author
+            </label>
+            <select
+              className="select-author"
+              id="postAuthor"
+              value={userId}
+              onChange={onAuthorChange}
+            >
+              <option value="">Select Author</option>
+              {usersOptions}
+            </select>
+          </div>
+          <div>
+            <label className="content-text" htmlFor="postContent">Content</label>
+            <textarea
+              name="postContent"
+              id="postContent"
+              value={content}
+              onChange={onContentChange}
+              cols="30"
+              rows="10"
+              className="content"
+            ></textarea>
+          </div>
+        </form>
+        <button className="content-btn" disabled={!canSave} onClick={onSavePostClicked} type="button">
+          Save Post
+        </button>
+      </div>
     </>
   );
 };
